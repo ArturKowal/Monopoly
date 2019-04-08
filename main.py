1c436342fc245ab8659d8a4cac64808016e343c0
The-bambi <krystian.figiel@smcebi.edu.pl>
2019-04-08 11:48:55 +0200

    Adding player object

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame
-
 pygame.init()
 
-screen = pygame.display.set_mode((720, 720))
-base_img = pygame.image.load("mapa.png")
+from screeninfo import get_monitors
+
+monitor = get_monitors()[0]
+
+resolution_height = monitor.height
+scrolling = False
+
+if resolution_height < 1024:
+    side_size = resolution_height - 72
+    scrolling = True
+    scrolling_height = side_size - 1024
+    scrolling_width = side_size - 1024
+
+screen = pygame.display.set_mode((side_size, side_size))
+base_img = pygame.image.load("mapa_z_miejscami_na_pionki.png")
 
 pygame.mixer.music.load("music1.mp3")
 pygame.mixer.music.play()
@@ -11,6 +23,8 @@ pygame.mixer.music.play()
 done = False
 
 while not done:
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
 
     if not pygame.mixer.music.get_busy():
         print('restart')
@@ -21,6 +35,38 @@ while not done:
             if event.key == pygame.K_SPACE:
                 done = True
 
+    if scrolling:
+        if mouse_x > side_size - 80:
+            scrolling_width -= 5
+        if mouse_x > side_size - 40:
+            scrolling_width -= 15
 
-    screen.blit(base_img,(0,0))
+        if mouse_x < 80:
+            scrolling_width += 5
+        if mouse_x < 40:
+            scrolling_width += 15
+
+        if mouse_y > side_size - 80:
+            scrolling_height -= 5
+        if mouse_y > side_size - 40:
+            scrolling_height -= 15
+
+        if mouse_y < 80:
+            scrolling_height += 5
+        if mouse_y < 40:
+            scrolling_height += 15
+
+    if scrolling_height < side_size - 1024:
+        scrolling_height = side_size - 1024
+    if scrolling_height > 0:
+        scrolling_height = 0
+
+    if scrolling_width < side_size - 1024:
+        scrolling_width = side_size - 1024
+    if scrolling_width > 0:
+        scrolling_width = 0
+
+
+    screen.fill((0,0,0))
+    screen.blit(base_img,(scrolling_width,scrolling_height))
     pygame.display.flip()
