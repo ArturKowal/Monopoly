0ade923cad770168b661ce7f1cf22bcb71845b6f
boojtus <wojciech.ciolczyk@smcebi.edu.pl>
2019-04-24 18:48:41 +0200

    1
    1

---
+++ pygameMenu/__init__.py
@@ -0,0 +1,15 @@
+
+from pygameMenu.menu import Menu
+from pygameMenu.textmenu import TextMenu
+import pygameMenu.fonts as fonts
+
+# Other
+__author__ = 'SM(onopoly)CBI'
+__contributors__ = [
+    'asierrayk',
+    'i96751414',
+    'ironsmile',
+    'maditnerd',
+    'Rifqi31',
+    'thisIsMikeKane',
+]
